fcd15a71aaa3927e3eb488264ce9d7480bd3285d
anton <anton@venshare.com>
2019-06-29 19:48:05 +0100

    remove logging

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const linuxPerf = require('linux-perf');
 const process = require('process');
 
 process.on('SIGUSR1', function () {
-  console.log('starting');
   linuxPerf.start();
 });
 
 process.on('SIGUSR2', function () {
   linuxPerf.stop();
-  console.log('finsi');
 });
